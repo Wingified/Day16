47732de6384b3cbfad686b1c989150b6e4f0ab3f
Brandon <86905571+Wingified@users.noreply.github.com>
2023-07-29 13:54:03 -0400

    Update main (15).py

--- main (15).py
+++ main (15).py
@@ -11,7 +11,7 @@ is_on = True
 while is_on:
     options = menu.get_items()
     choice = input(f"What would you like? ({options}): ")
-    if choice == "off":
+    if choice.lower() == "off":
         is_on = False
     elif choice == "report":
         coffee_maker.report()
